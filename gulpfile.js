53a019d63ad33966526dd61f34bd5eb8551bc24d
Austin Burdine <acburdine@gmail.com>
2015-05-13 17:21:50 -0600

    initial human commit - fixed .gitignore - added node stuff - started gulp script work

---
+++ gulpfile.js
@@ -0,0 +1,10 @@
+var gulp    = require('gulp'),
+    chalk   = require('chalk'),
+    less    = require('gulp-less'),
+    sass    = require('gulp-sass'),
+    concat  = require('gulp-concat'),
+    sourcemaps  = require('gulp-sourcemaps');
+
+gulp.task('less', function (cb) {
+    
+});
